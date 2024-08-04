15c27a16aefa6bb864af7a38ecaf9673af302767
Алексей Макеев <makeevhome@gmail.com>
2024-08-04 19:48:13 +0300

    9s

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/disign-galina",
-  assetsPrefix: "/disign-galina",
+  // basePath: "/disign-galina",
+  assetsPrefix: "/disign-galina/",
   output: "export",
   images: {
     unoptimized: true,
