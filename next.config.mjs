54862201bbb4c75cd7220cb0abe67c80ab2705fd
Алексей Макеев <makeevhome@gmail.com>
2024-08-05 00:10:31 +0300

    +++

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  basePath: "/disign-galina",
-  // assetPrefix: isProd ? "/disign-galina/" : undefined,
+  // basePath: "/disign-galina",
+  assetPrefix: isProd ? "/disign-galina/" : undefined,
   output: "export",
   // images: {
   //   unoptimized: true,
