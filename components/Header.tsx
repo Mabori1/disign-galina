87983eb84eb718c2c556b1698a39c317b5874104
Алексей Макеев <makeevhome@gmail.com>
2024-08-04 16:25:10 +0300

    поправил путь к лого

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ const nav = ["Главная", "Портфолио", "Услуги и цены",
 const Header = () => {
   return (
     <div className="sticky top-0 z-10 mx-auto flex w-full max-w-7xl items-center justify-between bg-white px-20 py-5">
-      <Image src="./logo.svg" alt="logo" width={382} height={82} />
+      <Image src="/logo.svg" alt="logo" width={382} height={82} />
       <div className="flex items-center justify-between gap-8">
         {nav.map((navItem, index) => (
           <Link
